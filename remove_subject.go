c2e0f0b31e1a96accc20c857c93997734d9f3a61
Alexander Kirillov <saratovsource@gmail.com>
2017-06-15 23:08:36 +0400

    Remove Subject

--- remove_subject.go
+++ remove_subject.go
@@ -1,7 +1,72 @@
 package kairgo
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type RemoveSubjectRequest struct {
+	SubjectID, GalleryName string //required fields
+	FaceID                 string //optional fields
+}
+
+func (r *RemoveSubjectRequest) IsValid() (bool, error) {
+	if r.SubjectID == "" {
+		return false, fmt.Errorf("SubjectID: shuld be required")
+	}
+
+	if r.GalleryName == "" {
+		return false, fmt.Errorf("GalleryName: shuld be required")
+	}
+
+	return true, nil
+}
+
+type ResponseRemoveSubject struct {
+	RawResponse []byte
+	Errors      []Error `json:"Errors"`
+	Status      string  `json:"status"`
+	Message     string  `json:"message"`
+}
+
 // RemoveSubject ...
-func (k *Kairos) RemoveSubject(subjectID, galleryName string) error {
-	// TODO
-	return nil
+func (k *Kairos) RemoveSubject(removeSubjectRequest *RemoveSubjectRequest) (*ResponseRemoveSubject, error) {
+	_, err := removeSubjectRequest.IsValid()
+	if err != nil {
+		return nil, err
+	}
+
+	p := map[string]interface{}{
+		"gallery_name": removeSubjectRequest.GalleryName,
+		"subject_id":   removeSubjectRequest.SubjectID,
+	}
+
+	if removeSubjectRequest.FaceID != "" {
+		p["face_id"] = removeSubjectRequest.FaceID
+	}
+
+	b, mErr := json.Marshal(p)
+	if mErr != nil {
+		return nil, mErr
+	}
+
+	req, reqErr := k.newRequest("POST", "gallery/remove_subject", b)
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	resp, doErr := k.do(req)
+	if doErr != nil {
+		return nil, doErr
+	}
+
+	re := &ResponseRemoveSubject{}
+
+	uErr := json.Unmarshal(resp, &re)
+	if uErr != nil {
+		return nil, uErr
+	}
+
+	re.RawResponse = resp
+	return re, nil
 }
